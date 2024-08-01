8c2e9dd57716e32cc20c4b68cd3494ba95d8c6cc
4imaN <like93860@gmail.com>
2024-08-01 21:28:21 +0300

    check if there is deleted file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,52 @@
 #!/usr/bin/python3
-"""
-Lockboxes challenge
-"""
+'''LockBoxes Challenge'''
+from sys import setrecursionlimit
+
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    initial_index = 0
+    keys = {0}
+    checkboxes(boxes, initial_index, keys)
+    zero_in = False
+    for i in boxes:
+        if 0 in i:
+            zero_in = True
 
-    Parameters
-    ----------
-    boxes : list of lists
-        A list where each index contains a list of keys available in that box.
+    if len(keys) > len(boxes) and zero_in:
+        return False
 
-    Returns
-    -------
-    bool
-        True if all boxes can be opened, else False.
-    """
-    n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    keys = [0]  
+    i = 0
+    while i < len(keys):
+        if i <= len(keys) - 2 and list(keys)[i + 1] != list(keys)[i] + 1:
+            return False
+        i += 1
+    return True
 
-    while keys:
-        new_key = keys.pop()
-        for key in boxes[new_key]:
-            if 0 <= key < n and not unlocked[key]:
-                unlocked[key] = True
-                keys.append(key)
-    
-    return all(unlocked)
+
+def checkboxes(boxes, pos, keys):
+    '''
+    The function "checkboxes" recursively checks if all checkboxes
+    can be selected based on the given positions and keys.
+    '''
+    setrecursionlimit(10000)
+    if len(keys) == len(boxes):
+        return True
+
+    # if calls >= len(boxes):
+    #     return keys
+
+    if pos >= len(boxes):
+        return
+
+    for i in boxes[pos]:
+        if i in keys:
+            continue
+        keys.add(i)
+        # print(f"pos = {pos}, i = {i}, keys = {keys}")
+        checkboxes(boxes, i, keys)
+    return keys
